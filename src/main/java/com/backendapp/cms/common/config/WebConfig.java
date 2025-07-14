4244b8a48a6524a0ca17aa122569fd44a1b61448
fajar-anggara <muhamadfajaranggara@gmail.com>
2025-07-14 15:49:54 +0700

    try to fix swagger

--- src/main/java/com/backendapp/cms/common/config/WebConfig.java
+++ src/main/java/com/backendapp/cms/common/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.backendapp.cms.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,6 +13,17 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*") // ubah ke domain tertentu jika perlu
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Explicit webjars handling
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        // Swagger UI specific
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/");
     }
 }
 
