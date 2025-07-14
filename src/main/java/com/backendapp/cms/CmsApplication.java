6dfc623cfec74c18a9f52f8b851a883dbfed0f2f
fajar-anggara <muhamadfajaranggara@gmail.com>
2025-07-14 13:10:32 +0700

    test build

--- src/main/java/com/backendapp/cms/CmsApplication.java
+++ src/main/java/com/backendapp/cms/CmsApplication.java
@@ -7,10 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class CmsApplication {
-	/**
-	* Kalau nanti sudah bisa ngirim email, is_email_vverified logic buat,
-	 * dan getRefreshPasswordToken buat agar refreshPasswordToken dikirim lewat email
-	**/
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
 	}
